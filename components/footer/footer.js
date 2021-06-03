0e14481e6f370e658afad63c21f496d52c41cfe5
Matias Fernandez <mfernandez@techinsights.com>
2021-06-03 16:33:46 -0300

    Implementing the sidebar.

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,7 +1,19 @@
 import styles from './footer.module.scss';
 
 function Footer() {
-    return <footer className={styles.footer}>Footer</footer>;
+    return <footer className={styles.footer}>
+      <ul className={styles.nav}>
+        <li className={styles.nav__item}><a href="#" className={styles.nav__link}>Find your dream home</a></li>
+        <li className={styles.nav__item}><a href="#" className={styles.nav__link}>Request proposal</a></li>
+        <li className={styles.nav__item}><a href="#" className={styles.nav__link}>Download home planner</a></li>
+        <li className={styles.nav__item}><a href="#" className={styles.nav__link}>Contact us</a></li>
+        <li className={styles.nav__item}><a href="#" className={styles.nav__link}>Submit your property</a></li>
+        <li className={styles.nav__item}><a href="#" className={styles.nav__link}>Come work with us!</a></li>
+      </ul>
+      <p className={styles.copyright}>
+        &copy;Copyright by Matias Fernandez. If you imitate the design I'm going to make you suffer and kill you in the end.
+      </p>
+    </footer>;
 }
 
 export default Footer;
--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,7 +1,9 @@
 import styles from './sidebar.module.scss';
 
 function Sidebar() {
-    return <div className={styles.sidebar}>Sidebar</div>;
+    return <div className={styles.sidebar}>
+      <button className={styles.btn}></button>
+    </div>;
 }
 
 export default Sidebar;
